87a8ae74f773bf7185e64707e7ea08211858927b
James King <elroy@elroyjetson.org>
2015-10-03 06:44:31 -0500

    Added symbolic linking commands

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 LOCALBIN="$HOME/bin"
-DOTFILES="$HOME/.dotfiles"
+DOTFILES="$HOME/Documents/dotfiles"
 
 if [ ! -d $LOCALBIN ]
 then
@@ -14,7 +14,12 @@ then
 	git clone https://github.com/rupa/z.git $LOCALBIN/z
 fi
 
-if [ ! -d $DOTFILES ]
-then
-	mkdir $DOTFILES
-fi
+# link files
+
+ln -s $DOTFILES/bash_profile $HOME/.bash_profile
+ln -s $DOTFILES/bash_aliases $HOME/.bash_aliases
+ln -s $DOTFILES/bash_prompt $HOME/.bash_prompt
+ln -s $DOTFILES/vimrc $HOME/.vimrc
+ln -s $DOTFILES/gitconfig $HOME/.gitconfig
+ln -s $DOTFILES/gitignore_global $HOME/.gitignore_global
+ln -s $DOTFILES/tmux.conf $HOME/.tmux.conf
